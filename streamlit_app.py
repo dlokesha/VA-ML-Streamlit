0a2f8d561d57df7e8de9f6eb736ea4b7cbf1091d
dlokesha <dhruthilokesh21@gmail.com>
2025-02-07 18:11:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,20 @@ import pandas as pd #to read the dataset (.csv)
 st.title('🤖 Machine Learning Application')
 st.info('This Application builds a Machine Learning Application using Streamlit!')
 
+#using expander - dropdown
 with st.expander('Data'):
   st.write('**Raw data**')
   df= pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df #to display the dataset
+
+  #adding variables
+  st.write('**X**')
+  X= df.drop('species' , axis =1)
+  X #display X
+
+  st.write('**y**')
+  y=df.species
+  y
+  
 
 
