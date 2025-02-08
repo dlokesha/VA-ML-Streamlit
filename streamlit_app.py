a98484b052a197356864ad70bf247010fdb085ce
dlokesha <dhruthilokesh21@gmail.com>
2025-02-08 09:37:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ with st.expander('Data Preparation'):
 
 ##Train the Ml MOdel
 clf = RandomForestClassifier()
-clf = clf.fit(X_raw, y)
+clf = clf.fit(x, y)
 
 ##Apply model to make Predictions
 prediction = clf.predict(input_row)
